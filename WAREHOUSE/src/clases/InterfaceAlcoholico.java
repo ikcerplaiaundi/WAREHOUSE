29b6e8981f8b09dd83e851fc5c9d7b68f7804469
IIII <IIII@DESKTOP-LQIO09A>
2023-01-02 14:01:06 +0100

    vino, en principio, terminado. IntefaceAlcoholico aonstructores añadidos

--- WAREHOUSE/src/clases/InterfaceAlcoholico.java
+++ WAREHOUSE/src/clases/InterfaceAlcoholico.java
@@ -1,18 +1,32 @@
 package clases;
 
-public abstract class InterfaceAlcoholico {
+public abstract class InterfaceAlcoholico {// ceveza o vino por lo que se necesita un boolean que indique si es un vino
+											
 	public static final double TASAA_BEBIDAS_FUERTES = 64.13;
 	public static final double TASAA_BEBIDAS_SUABES = 38.48;
-	
-	public boolean esFuerte(double graduacion,boolean vino) {
-		if(vino) {return (graduacion>13.5);}
-		else {return (graduacion>7);}	
+	boolean vino;
+	//si me dice que es un vino lo sera, si no, asume que no lo es
+	public InterfaceAlcoholico(boolean vino) {
+			this.vino = vino;
 	}
-	
-	public double calcularTasa(double graduacion,boolean vino) {
-		if(esFuerte(graduacion, vino)) {
+	public InterfaceAlcoholico() {
+		this.vino = false;
+	}
+
+	public boolean esFuerte(double graduacion) {
+		if (vino) {
+			return (graduacion > 13.5);
+		} else {
+			return (graduacion > 7);
+		}
+	}
+
+	public double calcularTasa(double graduacion) {
+		if (esFuerte(graduacion)) {
 			return TASAA_BEBIDAS_FUERTES;
-		}else {return TASAA_BEBIDAS_SUABES;}
+		} else {
+			return TASAA_BEBIDAS_SUABES;
+		}
 	}
 
 }
---
+++ WAREHOUSE/src/clases/Vino.java
@@ -0,0 +1,57 @@
+package clases;
+
+public class Vino extends InterfaceAlcoholico {
+//si es un vino le pasa el dato al interface^ en su etapa de contruccion vino true
+	public Vino(boolean vino) {
+		super(vino = true);
+	}
+
+//declaraciones
+	private String color;
+	private String origen;
+	private int anio;
+	private String tipoDeUva;
+	private double gradosAlcohol;
+
+//set get
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+
+	public int getAnio() {
+		return anio;
+	}
+
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+
+	public String getTipoDeUva() {
+		return tipoDeUva;
+	}
+
+	public void setTipoDeUva(String tipoDeUva) {
+		this.tipoDeUva = tipoDeUva;
+	}
+
+	public double getGradosAlcohol() {
+		return gradosAlcohol;
+	}
+
+	public void setGradosAlcohol(double gradosAlcohol) {
+		this.gradosAlcohol = gradosAlcohol;
+	}
+
+}
