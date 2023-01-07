9c44f3c1da9b037b03e15c329ba7140327dc827b
IIII <IIII@DESKTOP-LQIO09A>
2023-01-07 17:39:39 +0100

    factura tal ve?

--- WAREHOUSE/src/clases/Factura.java
+++ WAREHOUSE/src/clases/Factura.java
@@ -1,5 +1,28 @@
 package clases;
 
-public class Factura {
+import java.util.Date;
 
+public class Factura {
+	public static int iva =21;
+	private int numero;
+	private String nombreEmpresa;
+	private Date fecha;
+	private String concepto;
+	
+	public void addLinea(LineaFactura lineaFactura) {
+		
+	}
+	public void eliminarLinea(LineaFactura lineaFactura) {
+		
+	}
+	public double precioTotal(int cantidad) {
+		//precio*cantidad?
+		return 0;
+	}
+	public void  mostrarEnPantalla() {
+		
+	}
+	public void guardarEnFichero() {
+		
+	}
 }
