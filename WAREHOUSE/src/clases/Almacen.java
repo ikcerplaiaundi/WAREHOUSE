153f0a5e11d0ecdf39d574b6b8045cffe3323f9e
plaiaundi <plaiaundi@HZ309811.H012108.NET>
2023-01-11 13:50:09 +0100

    almacen al 51%

--- WAREHOUSE/src/clases/Almacen.java
+++ WAREHOUSE/src/clases/Almacen.java
@@ -6,53 +6,77 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Almacen {
+	public ArrayList<Articulo> lineaAlmacen = new ArrayList<Articulo>();
 
 	public void caragarDatos(String url) {
-		ArrayList<Articulo> lineaAlmacen = new ArrayList<Articulo>();
+
 		try {
 			File file = new File(url);
 			Scanner lectorFichero = new Scanner(file);
 			String linea;
-
+			int i = 0;
 			while (lectorFichero.hasNext()) {
 				linea = lectorFichero.nextLine();
 				String[] partes = linea.split(";");
-
+				String[] celerales = { partes[7], partes[8] };
+				i++;
 				switch (partes[0]) {
 				case "cerveza": {
-					
+
 					Cerveza cerveza = new Cerveza();
+					// comunes
 					cerveza.setCode(partes[1]);
 					cerveza.setMark(partes[2]);
-					cerveza.setCapacidadBotella(Integer.parseInt(partes[3]) );
+					cerveza.setCapacidadBotella(Integer.parseInt(partes[3]));
 					cerveza.setPrecio(Double.parseDouble(partes[4]));
 					cerveza.setStock(Integer.parseInt(partes[5]));
-					
-					
+					// alcohol
+					cerveza.setGradosAlcohol(Double.parseDouble(partes[6]));
+					cerveza.setOrigen(partes[7]);
+
+					// propios
+					cerveza.setCereales(celerales);
+
+					// meter articulo nuevo
 					lineaAlmacen.add(cerveza);
 				}
 				case "vino": {
 					Vino vino = new Vino();
+					// comunes
 					vino.setCode(partes[1]);
 					vino.setMark(partes[2]);
-					vino.setCapacidadBotella(Integer.parseInt(partes[3]) );
+					vino.setCapacidadBotella(Integer.parseInt(partes[3]));
 					vino.setPrecio(Double.parseDouble(partes[4]));
 					vino.setStock(Integer.parseInt(partes[5]));
-					
+					// alcohol
+					vino.setGradosAlcohol(Double.parseDouble(partes[6]));
+					vino.setOrigen(partes[7]);
+					// propios
+					vino.setColor(partes[8]);
+					vino.setAnio(Integer.parseInt(partes[9]));
+					vino.setTipoDeUva(partes[9]);
+
+					// meter articulo nuevo
 					lineaAlmacen.add(vino);
 				}
 				case "refresco": {
 					Refresco refresco = new Refresco();
+					// comunes
 					refresco.setCode(partes[1]);
 					refresco.setMark(partes[2]);
-					refresco.setCapacidadBotella(Integer.parseInt(partes[3]) );
+					refresco.setCapacidadBotella(Integer.parseInt(partes[3]));
 					refresco.setPrecio(Double.parseDouble(partes[4]));
 					refresco.setStock(Integer.parseInt(partes[5]));
-					
+					// propios
+					refresco.setSabor(partes[6]);
+					refresco.setZumo(Boolean.parseBoolean(partes[7]));
+					refresco.setGaseoso(Boolean.parseBoolean(partes[8]));
+					refresco.setCantidadAzucar(Integer.parseInt(partes[9]));
+					// meter articulo nuevo
 					lineaAlmacen.add(refresco);
 				}
 				default:
-					throw new IllegalArgumentException("Unexpected value: " + key);
+					System.out.println("dato de mierda en almacen linea" + i);
 				}
 
 			}
@@ -65,11 +89,24 @@ public class Almacen {
 	}
 
 	public Articulo elMasCaro() {
-		return;
+		double elMasCaro = 0;
+		int i = 0;
+		for (i = 0; lineaAlmacen.size() > i; i++) {
+			if (lineaAlmacen.get(i).getPrecio() < elMasCaro) {
+				elMasCaro = lineaAlmacen.get(i).getPrecio();
+			}
+		}
+		return lineaAlmacen.get(i);
 	}
 
 	public double precio(String codigoProducto) {
-		return 0;
+		double precio = -1;
+		for (int i = 0; lineaAlmacen.size() > i; i++) {
+			if (lineaAlmacen.get(i).getCode() == codigoProducto) {
+				precio = lineaAlmacen.get(i).getPrecio();
+			}
+		}
+		return precio;
 	}
 
 	public boolean hayStock(String codigoProducto) {
@@ -78,11 +115,19 @@ public class Almacen {
 	}
 
 	public ArrayList stockJusto() {
-		// return ArrayList
+
+		return lineaAlmacen;
 	}
 
-	public articulo (String codigoProducto) {
-		
+	public Articulo articulo(String codigoProducto) {
+
+		for (int i = 0; lineaAlmacen.size() > i; i++) {
+			if (lineaAlmacen.get(i).getCode() == codigoProducto) {
+				return lineaAlmacen.get(i);
+			}
+			
+		}return null;
+
 	}
 
 }
